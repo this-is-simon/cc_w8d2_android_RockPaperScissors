d186fdc60379b5a79ca427beff4e4093c1bfe2b9
Simon Atkins <this-is-simon@github.com>
2018-04-24 11:31:31 +0100

    Created gameLogic class

---
+++ app/src/main/java/rockpaperscissors/codeclan/com/rockpaperscissors/GameLogic.java
@@ -0,0 +1,29 @@
+package rockpaperscissors.codeclan.com.rockpaperscissors;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class GameLogic {
+
+    ArrayList<String> computerAnswers;
+
+    public GameLogic() {
+        setUpComputerAnswers();
+    }
+
+    public void setUpComputerAnswers(){
+        this.computerAnswers = new ArrayList();
+
+        computerAnswers.add("Rock");
+        computerAnswers.add("Paper");
+        computerAnswers.add("Scissors");
+
+    }
+
+    public ArrayList<String> getComputerAnswers(){ return new ArrayList<> (this.computerAnswers)};
+
+    public String getRandomAnswer(){
+        Collections.shuffle(this.computerAnswers);
+        return this.computerAnswers.get(0);
+    }
+}
---
+++ app/src/test/java/rockpaperscissors/codeclan/com/rockpaperscissors/GameLogicTest.java
@@ -0,0 +1,7 @@
+package rockpaperscissors.codeclan.com.rockpaperscissors;
+
+import static org.junit.Assert.*;
+
+public class GameLogicTest {
+
+}
